b9599c0b5a14aa7b26a86ed9285b8ed710b24a58
Alain <alainrodriguezz@gmail.com>
2020-12-31 17:10:45 -0500

    changed indentation to spaces

--- Fizz Buzz.js
+++ Fizz Buzz.js
@@ -14,18 +14,18 @@ var totalNumbers = 100;
 
 for (var i = 1; i <= totalNumbers; i++) 
 {
-	var isMultipleOf3 = checkIfItsMultiple(i,3);
-	var isMultipleOf5 = checkIfItsMultiple(i,5);
-	var result 		= i;
+  var isMultipleOf3 = checkIfItsMultiple(i,3);
+  var isMultipleOf5 = checkIfItsMultiple(i,5);
+  var result    = i;
 
-	if(isMultipleOf3) 									result = "Fizz";
-	if(isMultipleOf5) 									result = "Buzz";
-	if(isMultipleOf3 && isMultipleOf5) 	result	= "Fizz Buzz";
+  if(isMultipleOf3)                   result = "Fizz";
+  if(isMultipleOf5)                   result = "Buzz";
+  if(isMultipleOf3 && isMultipleOf5)  result  = "Fizz Buzz";
 
-	console.log(result);
+  console.log(result);
 }
 
 function checkIfItsMultiple(number,divisor) 
 {
-	return number % divisor == 0 ? true : false;
+  return number % divisor == 0 ? true : false;
 }
